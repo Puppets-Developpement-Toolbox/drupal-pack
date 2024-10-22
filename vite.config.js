53744cdceb6311606530b4c8f5b3659431b4878e
Samuel Laulhau <samuel.laulhau@puppets.fr>
2024-10-22 12:03:22 +0200

    fix viteconfig build root path

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,35 @@
-import { defineConfig } from 'vite'
-import liveReload from 'vite-plugin-live-reload'
+import { defineConfig } from "vite";
+import liveReload from "vite-plugin-live-reload";
 
-
-export default function pptsViteConfig({theme, root, port}) {
-  return defineConfig(({ mode }) => ({
-    plugins: [
-      liveReload(`${root}/web/themes/${theme}/**/*`)
-    ],
-    root: `${root}/web${mode !== 'development' ? '/themes/sodexo' : ''}`,
-    base: mode !== 'development' ? `/themes/${theme}/dist/` : '',
-    build: {
-      manifest: true,
-      sourcemap: mode == 'development',
-      outDir: `${root}/web/themes/${theme}/dist`,
-      rollupOptions: {
-        input: {
-          main: `${root}/web/themes/${theme}/js/main.js`,
-          global: `${root}/web/themes/${theme}/css/global.css`,
-          print: `${root}/web/themes/${theme}/css/print.css`,
-        }
+export default function pptsViteConfig({ theme, root, port }) {
+  return defineConfig(({ mode }) => {
+    const config = {
+      plugins: [liveReload(`${root}/web/themes/${theme}/**/*`)],
+      root: `${root}/web${mode !== "development" ? `/themes/${theme}` : ""}`,
+      base: mode !== "development" ? `/themes/${theme}/dist/` : "",
+      build: {
+        manifest: true,
+        sourcemap: mode == "development",
+        outDir: `${root}/web/themes/${theme}/dist`,
+        rollupOptions: {
+          input: {
+            main: `${root}/web/themes/${theme}/js/main.js`,
+            global: `${root}/web/themes/${theme}/css/global.css`,
+            print: `${root}/web/themes/${theme}/css/print.css`,
+          },
+        },
+        target: "es2018",
+        write: true,
       },
-      target: 'es2018',
-      write: true
-    },
-    server: {
-      port: port || 80,
-      host: true,
-    },
-    css: {
-      devSourcemap: mode == 'development'
-    }
-  }))
+      server: {
+        port: port || 80,
+        host: true,
+      },
+      css: {
+        devSourcemap: mode == "development",
+      },
+    };
+
+    return config;
+  });
 }
